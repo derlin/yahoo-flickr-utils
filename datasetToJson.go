7ce995202dcb7d8c94a5165b736db5cc7964df99
Lucy Linder <lucy.derlin@gmail.com>
2015-12-08 15:57:40 +0100

    multiple processes version: you can choose between multiple processes or a single program (default, when no argument)

--- datasetToJson.go
+++ datasetToJson.go
@@ -12,6 +12,8 @@ import (
     "errors"
     "strings"
     "log"
+    "math"
+    "strconv"
 )
 
 
@@ -44,10 +46,9 @@ type Photo struct {
     Width int       `xml:"width_o,attr" json:"width"`
 }
 
-type PhotoRawInfos struct {
+type JsonResult struct {
     Id string
-    Owner string
-    dateTaken string
+    Json string
 }
 
 
@@ -69,14 +70,37 @@ func CheckErr(err error) {
 
 // ------------------------------------------------
 
+
 func main(){
     logger = log.New(os.Stderr, "logger: ", log.Lshortfile)
 
-    if len(os.Args) != 2 {
-        ExitErr(fmt.Sprintf("usage: %s <path to data file> \n", os.Args[0]))
+    if len(os.Args) < 2 {
+        ExitErr(fmt.Sprintf("usage: %s <path to data file> [<nbr processes>]\n", os.Args[0]))
     }
 
-    file, err := os.Open(os.Args[1])
+    if len(os.Args) == 2 {
+        singleProcess(os.Args[1])
+
+    }else{
+        var nbrProcesses int
+        var err error
+
+        if nbrProcesses, err = strconv.Atoi(os.Args[2]); err != nil {
+            ExitErr(fmt.Sprintf("error: %s is not an int", os.Args[2]))
+        } 
+        
+        if nbrProcesses < 0 || nbrProcesses > 10 {
+            ExitErr("error: nbrProcesses must be between 1 and 10")
+        }
+
+        dispatcher(os.Args[1], nbrProcesses)
+    }
+}
+
+
+
+func singleProcess(filepath string) {
+    file, err := os.Open(filepath)
     CheckErr(err)
 
     defer file.Close()
@@ -113,6 +137,130 @@ func main(){
     log.Printf("STATS: lines = %d, ok = %d, errors = %d\n", lines, ok, errors)
 }
 
+
+
+func dispatcher(filepath string, nbrProcesses int){
+
+    in := make(chan string)
+    outOk := make(chan *JsonResult)
+    outError := make(chan error)
+
+
+    // launch processes
+    for i := 0; i < nbrProcesses; i++ {
+        go process(i, in, outOk, outError)
+    }
+
+    // launch reducer
+    totalLines := make(chan int64)
+    done := make(chan bool)
+    go reducer(outOk, outError, totalLines, done)
+
+    // read file and dispatch lines to processes
+    file, err := os.Open(os.Args[1])
+    CheckErr(err)
+    scanner := bufio.NewScanner(file)
+    var lines int64
+
+    for scanner.Scan() {
+        in <- scanner.Text()
+        log.Println("Master: dispatched 1 line")
+        lines++
+    }
+    totalLines <- lines
+    close(totalLines)
+
+    if err := scanner.Err(); err != nil {
+        CheckErr(err)
+    } 
+
+    file.Close()
+    log.Println("Master: dispatched jobs")
+
+    close(in)
+    // wait for the reducer to be done
+    <- done
+
+}
+
+
+func reducer(outOk chan *JsonResult, outError chan error, totalLines chan int64, done chan bool ) {
+    // gather results
+    var totalOks, totalErrors, results int64
+    
+    var lines, l int64
+    lines =  math.MaxInt64
+
+    for results = 0; results < lines; {
+        var ok bool
+
+        select {
+            case l, ok = <- totalLines:
+                if ok { 
+                    lines = l
+                    log.Printf("Reducer: total lines received: %d\n", lines)
+                }
+
+            case err, ok := <- outError:
+                if ok { 
+                    log.Println(err)
+                    totalErrors++
+                    results++
+                }
+
+            case res, ok := <- outOk:
+                if ok {
+                    fmt.Println(res.Json)
+                    log.Println(res.Id + " : OK")
+                    totalOks++
+                    results++
+                }
+
+            default:
+                ;
+        }
+    }
+
+    // close channels
+    close(outOk)
+    close(outError)
+
+    // finish
+    log.Printf("STATS: lines = %d, ok = %d, errors = %d\n", lines, totalOks, totalErrors)
+    done <- true
+}
+
+
+func process(jid int, in chan string, outOk chan *JsonResult, outError chan error) {
+
+    log.Printf("Job %d starting\n", jid)
+
+    for {
+
+        line, ok := <- in
+        if !ok {
+            log.Printf("Job %d finished\n", jid)
+            return
+        }
+
+        log.Printf("Job %d processing one line\n", jid)
+        tokens := strings.Fields(line)
+        id := tokens[0]
+        owner := tokens[1]
+        dateTaken := tokens[3] + " " + tokens[4][:len(tokens[4])-2]
+        j, err := getJson(id, owner, dateTaken)
+
+        if err != nil {
+            outError <- err
+
+        }else{
+            outOk <- &JsonResult{id, string(j)}
+        }
+
+        log.Printf("Job %d processed one line\n", jid)
+    }
+
+}
 
 // ------------------------------------------------
 
